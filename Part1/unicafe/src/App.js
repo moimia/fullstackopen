69386efd5cf7bc0ead161f0d49e44a20af74dd46
moimia <miasuomessa@gmail.com>
2023-04-30 14:01:19 +0300

    Unicafe step3

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -1,14 +1,48 @@
 import { useState } from 'react'
 
+// const App = () => {
+//   // save clicks of each button to its own state
+//   const [good, setGood] = useState(0)
+//   const [neutral, setNeutral] = useState(0)
+//   const [bad, setBad] = useState(0)
+
+//   const total = good + neutral + bad
+//   const averageScore = (good - bad) / total || 0
+//   const positivePercentage = (good * 100) / total || 0
+
+//   return (
+//     <div>
+//       <h1>give feedback</h1>
+//       <button onClick={() => setGood(good + 1)} >Good</button>
+//       <button onClick={() => setNeutral(neutral + 1)} >Neutral</button>
+//       <button onClick={() => setBad(bad + 1)} >Bad</button>
+
+//       <h1>statistics</h1>
+//       <p>Good: {good}</p>
+//       <p>Neutral: {neutral}</p>
+//       <p>Bad: {bad}</p>
+//       <p>Total feedbacks: {total}</p>
+//       <p>Average score: {averageScore}</p>
+//       <p>Positive percentage: {positivePercentage}</p>
+//     </div>
+//   )
+// }
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>Good: {props.good}</p>
+      <p>Neutral: {props.neutral}</p>
+      <p>Bad: {props.bad}</p>
+    </div>
+  )
+}
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const total = good + neutral + bad
-  const averageScore = (good - bad) / total || 0
-  const positivePercentage = (good * 100) / total || 0
 
   return (
     <div>
@@ -17,13 +51,7 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)} >Neutral</button>
       <button onClick={() => setBad(bad + 1)} >Bad</button>
 
-      <h1>statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>Total feedbacks: {total}</p>
-      <p>Average score: {averageScore}</p>
-      <p>Positive percentage: {positivePercentage}</p>
+      <Statistics good={good} />
     </div>
   )
 }
