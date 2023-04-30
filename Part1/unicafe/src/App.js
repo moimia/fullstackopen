2cc3cf40ae5eef00503e52d6978b8a23c27cb188
moimia <miasuomessa@gmail.com>
2023-04-30 13:32:34 +0300

    Unicafe Step2

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -6,17 +6,24 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const total = good + neutral + bad
+  const averageScore = (good - bad) / total || 0
+  const positivePercentage = (good * 100) / total || 0
+
   return (
     <div>
       <h1>give feedback</h1>
       <button onClick={() => setGood(good + 1)} >Good</button>
       <button onClick={() => setNeutral(neutral + 1)} >Neutral</button>
-      <button onClick={() => setBad(good + 1)} >Bad</button>
+      <button onClick={() => setBad(bad + 1)} >Bad</button>
 
       <h1>statistics</h1>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>Total feedbacks: {total}</p>
+      <p>Average score: {averageScore}</p>
+      <p>Positive percentage: {positivePercentage}</p>
     </div>
   )
 }
